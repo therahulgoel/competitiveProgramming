8f45c90a9f344947388b33b90088a6ae2c8a2b26
Rahul Goel <therahulgoel@gmail.com>
2017-08-06 21:56:04 +0530

    four basic matrix programs added

--- competitiveProgramming/competitiveProgramming/Matrix/matrix.hpp
+++ competitiveProgramming/competitiveProgramming/Matrix/matrix.hpp
@@ -11,4 +11,14 @@
 
 #include <stdio.h>
 
+
+//1. Read and print m*n matrix where m = number of rows, n = number of columns
+
+//2. Multiplication and sum of two matrix A and B
+
+//3. Print Diagonals of given input matrix of size m*n
+
+//4. Transpose of given matrix of size m*n
+
+
 #endif /* matrix_hpp */
