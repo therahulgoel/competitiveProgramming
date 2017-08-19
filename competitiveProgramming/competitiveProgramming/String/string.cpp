8bfb86b7f9f6fab31411431f397ca741c032e40c
Rahul Goel <therahulgoel@gmail.com>
2017-08-19 08:58:30 +0530

    basic string programs added

--- competitiveProgramming/competitiveProgramming/String/string.cpp
+++ competitiveProgramming/competitiveProgramming/String/string.cpp
@@ -7,3 +7,34 @@
 //
 
 #include "string.hpp"
+
+
+int string_numberOfchars(char *str){
+    int count =0;
+    
+    return count;
+}
+
+void string_reverseOfSentence(char *str){
+
+}
+
+void string_reverseOfString(char *str){
+
+}
+
+
+int string_frequencyOfEveryDistinctCharacter(char *str){
+    int freq =0;
+    return freq;
+}
+
+char string_maximumOccuringChar(char *str){
+    char ch;
+    return ch;
+}
+
+void string_allPermutationsOf(char *str){
+
+}
+
--- competitiveProgramming/competitiveProgramming/String/string.hpp
+++ competitiveProgramming/competitiveProgramming/String/string.hpp
@@ -12,18 +12,43 @@
 #include <stdio.h>
 
 //1. Number of characters in given string
+int string_numberOfchars(char *str);
 
 //2. Reverse of given string
+void string_reverseOfString(char *str);
 
 //3. Reverse of given sentence
+void string_reverseOfSentence(char *str);
 
 //4. Occurence/Frequency of every distinct character in given string
+int string_frequencyOfEveryDistinctCharacter(char *str);
 
 //5. Return maximum occuring character in given string
+char string_maximumOccuringChar(char *str);
 
 //6. To print all permutations of given string
+void string_allPermutationsOf(char *str);
 
-//7.
+//7. Paranenthesis chacker : for given expression check if given expression is in correct order of { } ( ) [] Print Balanced if yes or print not Balanced
+void string_paranthesisChecker(char *str);
+
+//8. Reverse words of given sentence
+void string_reverseWordsInGivenSentence(char *str);
+
+//9. Remove all adjacent duplicates from given string with recursion
+void string_removeAdjacentDuplicates(char *str);
+
+//10. Longest common subsequence in given String
+void string_lognestCommonSubsequence(char *str);
+
+//11. Longest common Substring in given string
+void string_longestCommonString(char *str);
+
+//12. Remove all the duplicates from given string With order of characetrs same as original order.
+void string_removeAllDuplicatesFrom(char *str);
+
+
+
 
 
 #endif /* string_hpp */
