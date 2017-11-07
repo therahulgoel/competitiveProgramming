23c5e5a4b0640cd692554730bcb31bde14edd431
Rahul Goel <therahulgoel@gmail.com>
2017-11-07 21:05:33 +0530

    string programs added

--- competitiveProgramming/competitiveProgramming/String/string.cpp
+++ competitiveProgramming/competitiveProgramming/String/string.cpp
@@ -1,6 +1,6 @@
 //
 //  string.cpp
-//  DS
+//  
 //
 //  Created by Rahul Goel on 7/26/17.
 //  Copyright © 2017 Rahul Goel. All rights reserved.
@@ -9,48 +9,128 @@
 #include "string.hpp"
 
 
-int string_numberOfchars(const char *str){
-    int count =0;
-    while (*str != '\0') {
-        count = count + 1;
-        str = str + 1;
-    }
-    return count;
-}
-
-void string_reverseOfSentence(char *str){
-}
-
-void string_reverseOfString(const char *str){
-    int count = string_numberOfchars(str);
-
-    while (count >= 0) {
-        printf("%c",str[count]);
-        count = count - 1;
-    }
-}
-
-
-int string_frequencyOfEveryDistinctCharacter(char *str){
-    int freq =0;
-    return freq;
-}
-
-char string_maximumOccuringChar(char *str){
-    char ch;
-    return ch;
-}
-
-void string_allPermutationsOf(char *str){
-
-    printf("%d",strlen(str));
-    for (int i =0; i< strlen(str)-1 ; i++) {
-        for (int j = i; j< strlen(str); j++) {
+void string_permutations( char *ch){
+    for (int i =0; i< strlen(ch); i++) {
+        for (int j = i; j< strlen(ch); j++) {
             
-            printf("%s",str);
-            printf("\n");
         }
     }
+}
+
+void string_frequencyOfEvryChar(char *str){
+    int frequency[26];
+    char checker[26];
+    int index = 0;
+    char ch = 'a';
+
+    //To initialize character array and respective frequency and store it in arrays
+    while (index < 26) {
+        checker[index] = ch;
+        frequency[index] = 0;
+        index++;
+        ch = ch + 1;
+    }
+    
+    index = 0;
+    int index2 = 0;
+    while (index< strlen(str)) {
+        index2 = 0;
+        while (index2 < 26) {
+            if (str[index] == checker[index2]) {
+                frequency[index2] = frequency[index2] + 1;
+            }
+            index2++;
+        }
+        index++;
+    }
+    
+    //to print frequency of each character
+    index = 0;
+    while (index<26) {
+        if (frequency[index] > 0) {
+            printf("%c",checker[index]);
+            printf("%d",frequency[index]);
+            printf("\n");
+        }
+        index++;
+    }
+}
+
+void string_adjacentDuplicate(char *str){
+    int i = 0;
+    while (i < strlen(str)) {
+        printf("%c",str[i]);
+        if (str[i] == str[i+1]) {
+            i = i + 2;
+        }else{
+            i = i + 1;
+        }
+    }
+}
+
+void printStringUpto(char *str,int start, int upto);
+void string_substringsAre(char *str,int start, int upto){
+    if (upto == strlen(str)){
+        printStringUpto(str,start,upto);
+        return;
+    }else{
+        if (start == strlen(str)) {
+            string_substringsAre(str ,0 ,upto+1);
+        }else{
+            string_substringsAre(str ,start + 1 ,upto);
+            printStringUpto(str,start,upto);
+        }
+    }
+}
+
+void printStringUpto(char *str,int start, int upto){
+    while (start <= upto) {
+        printf("%c",str[start]);
+        start++;
+    }
+    printf("\n");
+}
+
+void string_longestPalidromeSubstringIs(char *str){
     
 }
 
+void string_removeSpacesFromString(char str[]){
+    int count = 0,i = 0;
+    while (i<strlen(str)) {
+        if (str[i] != ' '){
+            str[count++] = str[i];
+        }
+        
+        i++;
+    }
+    str[count] = '\0';
+    printf("%s",str);
+}
+
+void string_checkIfPalindrome(char *str){
+    int found = 0;
+    int index = 0;
+    int length = (int)strlen(str);
+    while (index < length) {
+        if (str[index] != str[length - 1 - index]) {
+            found = 1;
+            break;
+        }
+        index++;
+    }
+    printf("%d",found);
+}
+
+int string_minCharsTomakeItPalindrome(char *str){
+    return 1;
+}
+
+
+void string_reverseArrayWithSpecialCharsFixed(char str[]){
+
+}
+
+void string_sentencePanagramOrNot(char *str){
+    
+}
--- competitiveProgramming/competitiveProgramming/String/string.hpp
+++ competitiveProgramming/competitiveProgramming/String/string.hpp
@@ -1,6 +1,6 @@
 //
 //  string.hpp
-//  DS
+//  
 //
 //  Created by Rahul Goel on 7/26/17.
 //  Copyright © 2017 Rahul Goel. All rights reserved.
@@ -8,48 +8,58 @@
 
 #ifndef string_hpp
 #define string_hpp
-
 #include <stdio.h>
+#include <stdlib.h>
 #include <string>
-
-//1. Number of characters in given string Time - O(n)
-int string_numberOfchars(const char *str);
-
-//2. Reverse of given string Time - O(n)
-void string_reverseOfString(const char *str);
-
-//3. Reverse of given sentence
-void string_reverseOfSentence(char *str);
-
-//4. Occurence/Frequency of every distinct character in given string
-int string_frequencyOfEveryDistinctCharacter(char *str);
-
-//5. Return maximum occuring character in given string
-char string_maximumOccuringChar(char *str);
-
-//6. To print all permutations of given string
-void string_allPermutationsOf(char *str);
-
-//7. Paranenthesis chacker : for given expression check if given expression is in correct order of { } ( ) [] Print Balanced if yes or print not Balanced
-void string_paranthesisChecker(char *str);
-
-//8. Reverse words of given sentence
-void string_reverseWordsInGivenSentence(char *str);
-
-//9. Remove all adjacent duplicates from given string with recursion
-void string_removeAdjacentDuplicates(char *str);
-
-//10. Longest common subsequence in given String
-void string_lognestCommonSubsequence(char *str);
-
-//11. Longest common Substring in given string
-void string_longestCommonString(char *str);
-
-//12. Remove all the duplicates from given string With order of characters same as original order.
-void string_removeAllDuplicatesFrom(char *str);
+#include <algorithm>
+using namespace std;
 
 
+//1. Permutation
+void string_permutations( char *ch);
 
+//2. Remove adjacent duplicate
+void string_adjacentDuplicate(char *str);
+
+//3. Frequency of every distinct character with char
+void string_frequencyOfEvryChar(char *str);
+
+//4. Check if given two strings are anagram
+void string_anagramOrNot(char *str1,char *str2);
+
+//5. Print all substrings of given string
+void string_substringsAre(char *str, int start, int upto);
+
+//6. Check if given string is Palindrome or not
+void string_checkIfPalindrome(char *str);
+
+//7. For given string find min number of characters to be inserted to convert it to palindrome.
+int string_minCharsTomakeItPalindrome(char *str);
+
+//8. For given string S, find longest palindrome sub-string
+void string_longestPalidromeSubstringIs(char *str);
+
+//9. Remove spaces from given string and return it
+void string_removeSpacesFromString(char str[]);
+
+//10. For given string, that contains special characters together with alphabets (a to z and A to Z), Reverse it with out affecting special characters
+void string_reverseArrayWithSpecialCharsFixed(char str[]);
+
+//11. Check if given sentence is Panagram (sentence containing all the characters from English alphabet) of not.
+void string_sentencePanagramOrNot(char *str);
+
+
+//Dynamic Progamming :
+
+
+//1. Longest Common SubString
+//Given two strings X and Y , find length of longest common substring.
+
+
+//2. Longest Common Subsequence
+//Given two strings X and Y , find length of longest common subsequence.
 
 
 #endif /* string_hpp */
+
+
