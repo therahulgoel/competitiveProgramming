65b9ba9085b3fad9f58af30ea83d068385583a85
Rahul Goel <therahulgoel@gmail.com>
2017-11-07 21:12:35 +0530

    matrix

--- competitiveProgramming/competitiveProgramming/Matrix/matrix.hpp
+++ competitiveProgramming/competitiveProgramming/Matrix/matrix.hpp
@@ -6,9 +6,23 @@
 //  Copyright © 2017 Rahul Goel. All rights reserved.
 //
 
-#ifndef matrix_hpp
-#define matrix_hpp
 
 #include <stdio.h>
 
+//Matrix to be used for test purposes
+static int matrixTest[][3] =  {{1,2,3},
+    {4,5,6},
+    {7,8,9}};
+
+
+//1. To Print given 2D matrix of size 3*3 Time - O(n)
+void matrix_2Dprint(int row, int col, int mat[][col]);
+
+//2. Search given element x, in a row wise and column wise sorted matrix of size n*n - Time- O(n)
+int matrix_searchInRowAndColumnWiseSorted(int row, int col, int mat[][col],int element);
+//3. Rotate given n*n matrix by 90 degree
+void matrix_2DRotate90Degree(int row, int col);
+
+//4. Print given matrix in spiral form
+void matrix_spiralForm(int row, int col, int mat[][col]);
 
